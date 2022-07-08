a0cb5aebb133ea750d4a51da00daee2b59937f4a
EC2 Default User <ec2-user@ip-172-31-41-20.ap-northeast-1.compute.internal>
2022-07-08 09:00:31 +0000

    [Add]kadai2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -28,7 +29,10 @@ class BooksController < ApplicationController
    
   end
 
-  def destoroy
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
    
   end
 
